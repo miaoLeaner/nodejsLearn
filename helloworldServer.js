30cd3a14b24e8493d53ac09bb66185457c9e080d
guu5tuu <guu5tuu@163.com>
2019-11-25 17:49:51 +0800

    复制家里电脑的内容

--- helloworldServer.js
+++ helloworldServer.js
@@ -1,1 +1,9 @@
 const http=require("http");
+var server=http.createServer(function(request,response){
+    console.log("a client connect");
+    console.log(request);
+    // console.log("url:"+request.url)
+    response.write("abc");
+    response.end();
+});
+server.listen(8181);
